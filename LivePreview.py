8185e15f2b933dfdfca6d5ffa3bed293e35d0fc3
Julien Bouquillon <julien@bouquillon.com>
2016-05-20 15:07:45 +0200

    electron ws: reconnect on failurewq

--- LivePreview.py
+++ LivePreview.py
@@ -10,12 +10,19 @@ class LivePreview(sublime_plugin.EventListener):
   def __init__(self, *args, **kwargs):
     """Initialize a new instance."""
     super().__init__(*args, **kwargs)
+    self._connect()
+
+  def _connect(self):
     self.ws = websocket.create_connection("ws://localhost:8099/socket")
 
   def on_activated(self, view):
     name = view.file_name()
     if name is not None:
-      self.ws.send(name)
+      try:
+        self.ws.send(name)
+      except BrokenPipeError as e:
+        print('BrokenPipeError: Electron server unreachable, try reconnect')
+        self._connect()
 
   # def on_deactivated(self, view):
   #   name = view.file_name()
